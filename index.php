76f269515ae01e01723f5017c40a2fa0303f7ffb
Alexios215 <alina031057@gmail.com>
2025-03-19 14:34:35 +0300

    slideshow added

--- index.php
+++ index.php
@@ -29,6 +29,30 @@ include 'db.php';
     <main>
         <h1>Добро пожаловать в наш магазин!</h1>
         <p>Здесь вы найдете лучшие товары по выгодным ценам.</p>
+        <div id="slideshow">
+            <div class="slide-wrapper">
+                <div class="slide">
+                    <img src="image1.jpg" alt="Хлеб">
+                    <p style="text-align: center;">Хлеб</p>
+                </div>
+                <div class="slide">
+                    <img src="image2.jpg" alt="Молоко">
+                    <p style="text-align: center;">Молоко</p>
+                </div>
+                <div class="slide">
+                    <img src="image3.jpg" alt="Гречка">
+                    <p style="text-align: center;">Гречка</p>
+                </div>
+                <div class="slide">
+                    <img src="image4.jpg" alt="Сахар">
+                    <p style="text-align: center;">Сахар</p>
+                </div>
+                <div class="slide">
+                    <img src="image5.jpg" alt="Мука">
+                    <p style="text-align: center;">Мука</p>
+                </div>
+            </div>
+        </div>
     </main>
     <footer>
         <p>&copy; 2025 Магазин. Все права защищены.</p>
